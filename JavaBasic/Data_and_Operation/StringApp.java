bd92714a06dad0c9f644bca94ec0a329b4c099bb
JungSeongCheol <kir5121@naver.com>
2020-11-14 22:19:06 +0900

    Java Operation(+, -).. and DataType

---
+++ JavaBasic/Data_and_Operation/StringApp.java
@@ -0,0 +1,20 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!"); // String
+		System.out.println('a'); //Character
+		System.out.println("H");
+		
+		System.out.println("Hello World");
+		
+		// new line
+		System.out.println("Hello \nWorld");
+		
+		// escape
+		System.out.println("Hello \"World\"");
+
+	}
+
+}
